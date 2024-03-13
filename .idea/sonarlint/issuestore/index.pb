3cd0b3d35178c48ff34267ea847e93eee39002f8
dbuelvasc <d.buelvasc@uniandes.edu.co>
2024-03-12 23:47:08 -0500

    created pantalla registrar

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+g
+7app/src/main/java/co/edu/uniandes/appsleep/registrar.kt,e\7\e76aab2be66cb4979aa53e2354aba141b46a7bb1
+c
+3app/src/main/java/co/edu/uniandes/appsleep/login.kt,b\f\bf3dc35e57bb1738417f2fe6fbebfeef38492467
+j
+:app/src/main/java/co/edu/uniandes/appsleep/MainActivity.kt,1\5\15fc313f36e00921a0f5dd4cf94db185b2ffbcea
--- app/src/main/java/co/edu/uniandes/appsleep/registrar.kt
+++ app/src/main/java/co/edu/uniandes/appsleep/registrar.kt
@@ -1,27 +1,20 @@
 package co.edu.uniandes.appsleep
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class registrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_registrar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registrar)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun abrirRegistrarActivity(view: View) {
-        val intent = Intent(this, registrar::class.java)
-        startActivity(intent)
     }
 }
